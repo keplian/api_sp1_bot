ef1abb5688f430174ef8940898dc36b06bf16605
keplian <keplian@gmail.com>
2021-04-27 00:50:04 +0300

    added get_homework_statuses and parse_homework_status

--- homework.py
+++ homework.py
@@ -9,46 +9,53 @@ load_dotenv()
 
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    homework_name = homework.get("homework_name")
+    if homework.get("status") == "rejected":
+        verdict = "К сожалению в работе нашлись ошибки."
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = (
+            "Ревьюеру всё понравилось, можно приступать к следующему уроку."
+        )
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    data = {"from_date": 1609525993}
+    headers = {"Authorization": f"OAuth {PRAKTIKUM_TOKEN}"}
+    homework_statuses = requests.get(
+        "https://praktikum.yandex.ru/api/user_api/homework_statuses/",
+        params=data,
+        headers=headers,
+    )
     return homework_statuses.json()
 
 
-def send_message(message, bot_client):
-    ...
-    return bot_client.send_message(...)
+# def send_message(message, bot_client):
+#     ...
+#     return bot_client.send_message(...)
 
 
-def main():
-    # проинициализировать бота здесь
-    current_timestamp = int(time.time())  # начальное значение timestamp
+# def main():
+#     # проинициализировать бота здесь
+#     current_timestamp = int(time.time())  # начальное значение timestamp
 
-    while True:
-        try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+#     while True:
+#         try:
+#             new_homework = get_homework_statuses(current_timestamp)
+#             if new_homework.get('homeworks'):
+#                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+#             current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
+#             time.sleep(300)  # опрашивать раз в пять минут
 
-        except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+#         except Exception as e:
+#             print(f'Бот столкнулся с ошибкой: {e}')
+#             time.sleep(5)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
